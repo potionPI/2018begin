02012865eea2d3adfb61e0f128c955b1c2101a16
Call me Potion Pi! <potion080pi@gmail.com>
2018-08-24 18:11:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function showProjects() {
+	var projectContent = document.getElementById("projects-content");
+	if (projectContent.style.display === "none") {
+		projectContent.style.display = "block";
+	} else {
+		projectContent.style.display = "none";
+	}
+}
+
+function showAbout() {
+	var aboutContent = document.getElementById("about-content");
+	if (aboutContent.style.display === "none") {
+		aboutContent.style.display = "block";
+	} else {
+		aboutContent.style.display = "none";
+	}
+}
+
+function showGearboxOfAFan() {
+	var gearBoxOfAFanContent = document.getElementById("gearbox-of-a-fan-content");
+	if (gearBoxOfAFanContent.style.display === "none") {
+		gearBoxOfAFanContent.style.display = "block";
+	} else {
+		gearBoxOfAFanContent.style.display = "none";
+	}
+}
+
+function showWaterwheel() {
+	var waterwheelContent = document.getElementById("waterwheel-content");
+	if (waterwheelContent.style.display === "none") {
+		waterwheelContent.style.display = "block";
+	} else {
+		waterwheelContent.style.display = "none";
+	}
+}
